1dd8b78db93b54136bf9d8d0bd91279929f74348
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2024-07-27 23:39:27 +0900

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const isDev = import.meta.env.DEV;
+
+const siteSettings = isDev ? {} : {
+  site: 'https://topgate.github.io',
+  base: '/tech-log-nebula',
+}
+
 // https://astro.build/config
 export default defineConfig({
+  ...siteSettings,
   integrations: [tailwind()]
-});
+})
